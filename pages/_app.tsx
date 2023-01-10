9a15826545e34076fd03b79ba3fec18588032df7
noriko-terada <terada@virtual-tech.net>
2023-01-11 00:22:09 +0900

    console.log追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log(`[App] reCAPTCHA SITE KEY(NEXT_PUBLIC)=${process.env.NEXT_PUBLIC_RECAPTCHA_KEY}`)
+  console.log(`[App] VTECXNEXT_URL (NEXT_PUBLIC)=${process.env.NEXT_PUBLIC_VTECXNEXT_URL}`)
   return (
     <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY!} language="ja">
       <Component {...pageProps} />
